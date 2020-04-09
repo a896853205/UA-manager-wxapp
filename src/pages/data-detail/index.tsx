b6280c6371e88c7e83d9a1eafd8048d3996b8892
Qccccc's laptop <896853205@qq.com>
2020-04-09 22:42:00 +0800

    feat: 添加统计页面基本内容

--- src/pages/data-detail/index.tsx
+++ src/pages/data-detail/index.tsx
@@ -1,6 +1,6 @@
 import Taro, { useState, useEffect, memo } from '@tarojs/taro';
 import { View, Picker, Text } from '@tarojs/components';
-import { AtTabs, AtTabsPane, AtButton } from 'taro-ui';
+import { AtTabs, AtTabsPane, AtButton, AtList, AtListItem } from 'taro-ui';
 import { useSelector } from '@tarojs/redux';
 import Chart from 'taro-echarts';
 
@@ -9,6 +9,7 @@ import './data-detail.css';
 
 interface IMeasure {
   measureText: string;
+  measureType: string;
 }
 interface IStatus {
   measure: IMeasure;
@@ -20,7 +21,7 @@ const TAB_LIST = [{ title: '折线' }, { title: '统计' }];
 const DataDetail = () => {
   const [timeSpanIndex, setTimeSpanIndex] = useState(0);
   const [tabIndex, setTabIndex] = useState(0);
-  const { measureText } = useSelector<IStatus, IMeasure>(
+  const { measureText, measureType } = useSelector<IStatus, IMeasure>(
     (state) => state.measure
   );
 
@@ -54,31 +55,110 @@ const DataDetail = () => {
               <Text className="picker-value">{TIME_RANGE[timeSpanIndex]}</Text>
             </View>
           </Picker>
-          {/* FIXME: 这里根据redux中选择哪个chart Components */}
-          <Chart
-            chartId={'1'}
-            option={{
-              grid: {
-                left: '50px',
-                right: '50px',
-              },
-              xAxis: {
-                type: 'category',
-                data: ['一', '二', '三', '四', '五', '六', '日'],
-                name: '星期',
-              },
-              yAxis: {
-                type: 'value',
-                name: '微摩尔',
-              },
-              series: [
-                {
-                  data: [160, 351, 652, 352, 849, 352, 849],
-                  type: 'line',
+          {/* FIXME: 需要将图表组件提出 */}
+          {measureType === 'single' ? (
+            <Chart
+              chartId={'1'}
+              option={{
+                grid: {
+                  left: '50px',
+                  right: '50px',
                 },
-              ],
-            }}
-          />
+                xAxis: {
+                  type: 'category',
+                  data: ['一', '二', '三', '四', '五', '六', '日'],
+                  name: '星期',
+                },
+                yAxis: {
+                  type: 'value',
+                  name: '微摩尔',
+                },
+                series: [
+                  {
+                    data: [160, 351, 652, 352, 849, 352, 849],
+                    type: 'line',
+                  },
+                ],
+              }}
+            />
+          ) : null}
+          {measureType === 'joint' ? (
+            <View>
+              {/* 每个图表需要一个标题 */}
+              <Chart
+                chartId={'1'}
+                option={{
+                  grid: {
+                    left: '50px',
+                    right: '50px',
+                  },
+                  xAxis: {
+                    type: 'category',
+                    data: ['一', '二', '三', '四', '五', '六', '日'],
+                    name: '星期',
+                  },
+                  yAxis: {
+                    type: 'value',
+                    name: '微摩尔',
+                  },
+                  series: [
+                    {
+                      data: [160, 351, 652, 352, 849, 352, 849],
+                      type: 'line',
+                    },
+                  ],
+                }}
+              />
+              <Chart
+                chartId={'1'}
+                option={{
+                  grid: {
+                    left: '50px',
+                    right: '50px',
+                  },
+                  xAxis: {
+                    type: 'category',
+                    data: ['一', '二', '三', '四', '五', '六', '日'],
+                    name: '星期',
+                  },
+                  yAxis: {
+                    type: 'value',
+                    name: '微摩尔',
+                  },
+                  series: [
+                    {
+                      data: [160, 351, 652, 352, 849, 352, 849],
+                      type: 'line',
+                    },
+                  ],
+                }}
+              />
+              <Chart
+                chartId={'1'}
+                option={{
+                  grid: {
+                    left: '50px',
+                    right: '50px',
+                  },
+                  xAxis: {
+                    type: 'category',
+                    data: ['一', '二', '三', '四', '五', '六', '日'],
+                    name: '星期',
+                  },
+                  yAxis: {
+                    type: 'value',
+                    name: '微摩尔',
+                  },
+                  series: [
+                    {
+                      data: [160, 351, 652, 352, 849, 352, 849],
+                      type: 'line',
+                    },
+                  ],
+                }}
+              />
+            </View>
+          ) : null}
         </View>
         <AtButton
           type="primary"
@@ -92,8 +172,134 @@ const DataDetail = () => {
         </AtButton>
       </AtTabsPane>
       <AtTabsPane current={1} index={1}>
-        <View style="padding: 100px 50px;background-color: #FAFBFC;text-align: center;">
-          标签页二的内容
+        <View>
+          {measureType === 'single' ? (
+            <View>
+              <View>尿酸</View>
+              <Chart
+                chartId={'1'}
+                option={{
+                  series: [
+                    {
+                      data: [
+                        {
+                          // 数据项的名称
+                          name: '偏高',
+                          // 数据项值8
+                          value: 10,
+                        },
+                        {
+                          name: '正常',
+                          value: 20,
+                        },
+                      ],
+                      type: 'pie',
+                    },
+                  ],
+                }}
+              />
+              <View className="at-row">
+                <View className="at-col">偏高 20 次</View>
+                <View className="at-col">正常 6 次</View>
+              </View>
+              <View className="at-row">
+                <View className="at-col">总共 20 次</View>
+                <View className="at-col">连续高位 6 次</View>
+              </View>
+              <View className="at-row">
+                <View className="at-col">最高 652 mmol/L</View>
+                <View className="at-col">最长高位 6 天</View>
+              </View>
+            </View>
+          ) : null}
+          {measureType === 'joint' ? (
+            <View>
+              {/* 每个图表需要一个标题 */}
+              <Chart
+                chartId={'1'}
+                option={{
+                  grid: {
+                    left: '50px',
+                    right: '50px',
+                  },
+                  xAxis: {
+                    type: 'category',
+                    data: ['一', '二', '三', '四', '五', '六', '日'],
+                    name: '星期',
+                  },
+                  yAxis: {
+                    type: 'value',
+                    name: '微摩尔',
+                  },
+                  series: [
+                    {
+                      data: [160, 351, 652, 352, 849, 352, 849],
+                      type: 'pie',
+                    },
+                  ],
+                }}
+              />
+              <Chart
+                chartId={'1'}
+                option={{
+                  grid: {
+                    left: '50px',
+                    right: '50px',
+                  },
+                  xAxis: {
+                    type: 'category',
+                    data: ['一', '二', '三', '四', '五', '六', '日'],
+                    name: '星期',
+                  },
+                  yAxis: {
+                    type: 'value',
+                    name: '微摩尔',
+                  },
+                  series: [
+                    {
+                      data: [
+                        {
+                          // 数据项的名称
+                          name: '数据1',
+                          // 数据项值8
+                          value: 10,
+                        },
+                        {
+                          name: '数据2',
+                          value: 20,
+                        },
+                      ],
+                      type: 'pie',
+                    },
+                  ],
+                }}
+              />
+              <Chart
+                chartId={'1'}
+                option={{
+                  grid: {
+                    left: '50px',
+                    right: '50px',
+                  },
+                  xAxis: {
+                    type: 'category',
+                    data: ['一', '二', '三', '四', '五', '六', '日'],
+                    name: '星期',
+                  },
+                  yAxis: {
+                    type: 'value',
+                    name: '微摩尔',
+                  },
+                  series: [
+                    {
+                      data: [160, 351, 652, 352, 849, 352, 849],
+                      type: 'pie',
+                    },
+                  ],
+                }}
+              />
+            </View>
+          ) : null}
         </View>
       </AtTabsPane>
     </AtTabs>
