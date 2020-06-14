a333ba199edcd3ecf1ca960a77912052a8df67f4
Zbr1920410104 <765451419@qq.com>
2020-06-14 19:20:23 +0800

    feat: 添加登录loading和密码错误提示

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,6 +1,6 @@
 import Taro, { memo, useState } from '@tarojs/taro';
 import { View } from '@tarojs/components';
-import { AtInput, AtForm, AtButton } from 'taro-ui';
+import { AtInput, AtForm, AtButton, AtMessage } from 'taro-ui';
 import md5 from 'md5';
 
 import { AUTHORIZE } from '../../constants/api-constants';
@@ -9,29 +9,44 @@ import http from '../../util/http';
 const Login = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [loginLoading, setLoginLoading] = useState(false);
 
   const authorize = () => {
-    http({
-      url: AUTHORIZE,
-      method: 'POST',
-      data: {
-        username,
-        password: md5(password),
-      },
-    }).then((res) => {
-      if (res.statusCode === 500) {
-        console.log('密码错误');
-      } else if (res.statusCode === 200) {
-        Taro.setStorageSync('token', res.data.data.token);
-        Taro.redirectTo({
-          url: '../../pages/index/index',
-        });
-      }
-    });
+
+    if (!loginLoading) {
+      setLoginLoading(true);
+
+      http({
+        url: AUTHORIZE,
+        method: 'POST',
+        data: {
+          username,
+          password: md5(password),
+        },
+      }).then((res) => {
+        if (res.statusCode === 500) {
+          Taro.atMessage({
+            'message': '密码错误',
+            'type': 'error',
+          });
+          console.log('密码错误');
+          setLoginLoading(false);
+        } else if (res.statusCode === 200) {
+          Taro.setStorageSync('token', res.data.data.token);
+          Taro.redirectTo({
+            url: '../../pages/index/index',
+          });
+          setLoginLoading(false);
+        }
+      });
+
+    }
+
   };
 
   return (
     <View>
+      <AtMessage />
       <AtForm>
         <AtInput
           name="value2"
@@ -56,6 +71,7 @@ const Login = () => {
         <AtButton
           full
           type="primary"
+          loading={loginLoading}
           onClick={() => {
             authorize();
           }}
