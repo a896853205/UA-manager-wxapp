6932102d0f333dd042c1e599d9ea3d6c65e2ce6a
Zbr1920410104 <765451419@qq.com>
2020-09-16 10:28:47 +0800

    feat(完成新闻下拉部分样式的修改，以及服务器域名修改):

--- src/constants/api-constants.ts
+++ src/constants/api-constants.ts
@@ -1,6 +1,6 @@
 const DominConfigs = {
   // DOMAIN: 'http://im-here.cn:3222/wx-volunteer',
-  DOMAIN: 'https://uricapi.im-here.cn/wx-volunteer',
+  DOMAIN: 'https://uricapi.jiarry.com/wx-volunteer',
 };
 
 export const AUTHORIZE = `${DominConfigs.DOMAIN}/authorize`;
